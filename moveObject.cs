cc45ea8c1e4c940bb7b36ba0f50782e63052c5a4
Salahiev <92461234+Salahiev@users.noreply.github.com>
2021-10-14 21:24:23 +0500

    Add files via upload

---
+++ moveObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent] // один экземпляр на объекте
+public class moveObject : MonoBehaviour
+{
+    [SerializeField] Vector3 movePosition;
+    [SerializeField] float moveSpeed;
+    [SerializeField] [Range(0,1)] float moveProgress; // 0-не двигался, 1-полностью переместился
+    Vector3 starPosition;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        starPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveProgress = Mathf.PingPong(Time.time*moveSpeed, 1);       // класс математический 
+        Vector3 offset = movePosition * moveProgress;
+        transform.position = starPosition + offset;
+
+    }
+}
